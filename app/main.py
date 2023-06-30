2fb61313148b67d519c3750732b957f4ed08e7c4
Hakgyu Kim <zage4448@gmail.com>
2023-06-30 09:25:18 +0900

    init: [김학규] 초기 CD 구성

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["http://3.37.125.157","http://localhost:8080"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
